f8fcd75880159aa0f1383713427e5f77ca1ccf7d
blah-this-blah-that <yugesh.kumar@gmail.com>
2013-03-25 20:09:10 +0530

    Create Constants.cs

---
+++ Constants.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ParentStatusChangeGenericPlugin
+{
+    public static class Constants
+    {
+        public const string SETSTATEDYNAMICENTITY = "setstatedynamicentity";
+        public const string SETSTATE = "setstate";
+        public const string ENTITYMONIKER = "EntityMoniker";
+        public const string STATE = "State";
+        public const string CONTACTID = "contactid";
+    }
+}
