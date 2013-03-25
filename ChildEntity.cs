9ba0c8de07cabd119f1dfe2d98146d8fe8b39f36
blah-this-blah-that <yugesh.kumar@gmail.com>
2013-03-25 20:07:59 +0530

    Update ChildEntity.cs

--- ChildEntity.cs
+++ ChildEntity.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Majestic.EntityAndContact.StatusChange
+namespace ParentStatusChangeGenericPlugin
 {
     public class ChildEntity
     {
